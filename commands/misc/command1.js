de389a25fa0d180f36eb0471530b01fb2a6461be
Grav <75702884+Gravxd@users.noreply.github.com>
2020-12-09 18:14:53 +0000

    Not Needed

--- commands/misc/command1.js
+++
@@ -1,9 +0,0 @@
-const { getMember, formatDate, promptMessage } = require("../../functions.js"); // common ways to do commands
-const { RichEmbed } = require("discord.js");
-module.exports = {
-    name: "commandname",
-    run: async (client, message, args) => {
-
-
-    }
-}
